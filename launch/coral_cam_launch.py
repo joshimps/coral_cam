655880ecfc4cf5ee5ead934c121783b2a06e6450
josh <joshimpey10@gmail.com>
2024-05-02 16:34:35 +1000

    updated post processing filters

--- launch/coral_cam_launch.py
+++ launch/coral_cam_launch.py
@@ -48,6 +48,8 @@ def generate_launch_description():
                           'pointcloud.ordered_pc':'true',
                           'enable_sync':'true',
                           'clip_distance' : '0.8',
+                          'decimation_filter.enable':'true',
+                          'spatial_filter.enable':'true',
                           'hole_filling_filter.enable':'false',
                          }.items()
         )
