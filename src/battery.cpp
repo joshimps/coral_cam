f8e399ef49019d172822e28b46cf9e3ac31d6561
josh <joshimpey10@gmail.com>
2024-05-20 11:22:32 +1000

    added publishers to battery and temp

--- src/battery.cpp
+++ src/battery.cpp
@@ -4,6 +4,7 @@ namespace coral_cam
 {
     Battery::Battery(const rclcpp::NodeOptions &options) : Node("battery_node", options){
         //Every minute we will publish the battery percentage
+        current_battery_publisher_ = this->create_publisher<std_msgs::msg::String>("current_battery_level", 10);
         timer_ = this->create_wall_timer(60000ms, std::bind(&Battery::ReadBatteryPercentage, this));
 
         //Ideally we would set up the battery reading here
@@ -13,7 +14,10 @@ namespace coral_cam
         //Here we would read the battery percentage from the sensor then publish it.
         //For now we will say the battery percentage is unknown
 
+        std_msgs::msg::String message;
         battery_level_ = "UNKNOWN";
+        message.data = battery_level_;
+        current_battery_publisher_->publish(message);
     }
 
 }
--- src/temperature_sensor.cpp
+++ src/temperature_sensor.cpp
@@ -4,6 +4,7 @@ namespace coral_cam
 {
     TemperatureSensor::TemperatureSensor(const rclcpp::NodeOptions &options) : Node("battery_node", options){
         //Every 5s we will publish the current temperature
+        current_temperature_publisher_ = this->create_publisher<std_msgs::msg::String>("current_temperature", 10);
         timer_ = this->create_wall_timer(5000ms, std::bind(&TemperatureSensor::ReadCurrentTemperature, this));
 
         //Ideally we would set up the battery reading here
@@ -13,7 +14,10 @@ namespace coral_cam
         //Here we would read the battery percentage from the sensor then publish it.
         //For now we will say the battery percentage is unknown
 
+        std_msgs::msg::String message;
         current_temperature_ = "UNKNOWN";
+        message.data = current_temperature_;
+        current_temperature_publisher_->publish(message);
     }
 
 }
